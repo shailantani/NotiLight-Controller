45fddcb1d574baf2854bb117f2d9e9b93a0b0b43
shailantani <shailantani04@gmail.com>
2026-02-13 20:21:44 +0530

    Initial commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,48 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `start` command */
+  export type Start = ExtensionPreferences & {}
+  /** Preferences accessible in the `stop` command */
+  export type Stop = ExtensionPreferences & {}
+  /** Preferences accessible in the `toggle` command */
+  export type Toggle = ExtensionPreferences & {}
+  /** Preferences accessible in the `clear` command */
+  export type Clear = ExtensionPreferences & {}
+  /** Preferences accessible in the `test` command */
+  export type Test = ExtensionPreferences & {}
+  /** Preferences accessible in the `settings` command */
+  export type Settings = ExtensionPreferences & {}
+  /** Preferences accessible in the `add` command */
+  export type Add = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `start` command */
+  export type Start = {}
+  /** Arguments passed to the `stop` command */
+  export type Stop = {}
+  /** Arguments passed to the `toggle` command */
+  export type Toggle = {}
+  /** Arguments passed to the `clear` command */
+  export type Clear = {}
+  /** Arguments passed to the `test` command */
+  export type Test = {}
+  /** Arguments passed to the `settings` command */
+  export type Settings = {}
+  /** Arguments passed to the `add` command */
+  export type Add = {}
+}
+
